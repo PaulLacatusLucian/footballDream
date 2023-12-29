0c0ba11ff235969fa0879cc4e3042d809fc1724e
Paul <71145854+PaulLacatusLucian@users.noreply.github.com>
2023-12-30 00:21:59 +0200

    day3

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    # host =
+    # topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_created=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
+    nr_of_participants = models.IntegerField(default=0)
+    current_participants = models.IntegerField(default=0)
+    time_of_game = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
